a5b40334565cfdfe689046111fdc91d0431ab48f
JSai S <jsai23@lawn-143-215-117-39.lawn.gatech.edu>
2022-06-21 10:52:33 -0400

    stripe now

--- src/components/onboarding.js
+++ src/components/onboarding.js
@@ -5,7 +5,6 @@ import {Navigate, useNavigate} from 'react-router-dom';
 
 const Onboarding = () => {
   const [inputs, setInputs] = React.useState({});
-  const [submitted, setSubmitted] = React.useState(false)
 
   const handleChange = (event) => {
     const name = event.target.name;
@@ -18,8 +17,6 @@ const Onboarding = () => {
     let temp = JSON.stringify(inputs)
       axios.post(link,temp).then(res => {
         console.log(res);
-        console.log(res.data);
-        setInputs(true)
       })
   }
 
@@ -49,79 +46,69 @@ const Onboarding = () => {
       }
     }, [user]);
   
-    if(user && !setInputs)
-    {
-      return (
-        <div>
-        <form onSubmit={handleSubmit}>
-          <Blank
-            label="Please enter your Client_ID"
-            name="client_id"
-            value={inputs.client_id || ""}
-            onChange={handleChange}
-          />
-          <Blank
-            label="Please enter your Client_Secret"
-            name="client_secret"
-            value={inputs.client_secret || ""}
-            onChange={handleChange}
-          />
-          <Blank
-            label="Please enter your Refresh_Token"
-            name="refresh_token"
-            value={inputs.refresh_token || ""}
-            onChange={handleChange}
-          />
-          <Dropdown
-            label="How much are you able to invest?"
-            options={[
-              {label: '', value: 0},
-              { label: 'Tier1', value: 500 },
-              { label: 'Tier2', value: 1000 },
-              { label: 'Tier3', value: 2000 },
-            ]}
-            name="investment"
-            value={inputs.investment || ""}
-            onChange={handleChange}
-          />
-          <Dropdown
-            label="How do you feel about risk on capital?"
-            options={[
-              {label: '', value: ''},
-              { label: 'Risk Averse', value: 'Low' },
-              { label: 'Risk Moderate', value: 'Medium' },
-              { label: 'Risk Inclined', value: 'High' },
-            ]}
-            name="risk_aversity"
-            value={inputs.risk_aversity || ""}
-            onChange={handleChange}
-          />
-          <BlankN
-            label="Product Price Minimum"
-            name="ppl"
-            value={inputs.ppl || 0}
-            onChange={handleChange}
-          />
-          <BlankN
-            label="Product Price Maximum"
-            name="pph"
-            value={inputs.pph || 0}
-            onChange={handleChange}
-          />
-          <p>{inputs.client_id},{inputs.client_secret},{inputs.refresh_token},{inputs.investment},{inputs.risk_aversity},{inputs.ppl},{inputs.pph}</p>
-    
-          <input type="submit" />
-        </form>
-        </div>
-      );
-    }
-    else if(user&&inputs)
-    {
-      return <Navigate to="/dashboard"/>
-    }
-    else{
-        return <Navigate to="/login"/>
-    }
+
+  return(
+      <form onSubmit={handleSubmit}>
+        <Blank
+          label="Please enter your Client_ID"
+          name="client_id"
+          value={inputs.client_id || ""}
+          onChange={handleChange}
+        />
+        <Blank
+          label="Please enter your Client_Secret"
+          name="client_secret"
+          value={inputs.client_secret || ""}
+          onChange={handleChange}
+        />
+        <Blank
+          label="Please enter your Refresh_Token"
+          name="refresh_token"
+          value={inputs.refresh_token || ""}
+          onChange={handleChange}
+        />
+        <Dropdown
+          label="How much are you able to invest?"
+          options={[
+            {label: '', value: 0},
+            { label: 'Tier1', value: 500 },
+            { label: 'Tier2', value: 1000 },
+            { label: 'Tier3', value: 2000 },
+          ]}
+          name="investment"
+          value={inputs.investment || ""}
+          onChange={handleChange}
+        />
+        <Dropdown
+          label="How do you feel about risk on capital?"
+          options={[
+            {label: '', value: ''},
+            { label: 'Risk Averse', value: 'Low' },
+            { label: 'Risk Moderate', value: 'Medium' },
+            { label: 'Risk Inclined', value: 'High' },
+          ]}
+          name="risk_aversity"
+          value={inputs.risk_aversity || ""}
+          onChange={handleChange}
+        />
+        <BlankN
+          label="Product Price Minimum"
+          name="ppl"
+          value={inputs.ppl || 0}
+          onChange={handleChange}
+        />
+        <BlankN
+          label="Product Price Maximum"
+          name="pph"
+          value={inputs.pph || 0}
+          onChange={handleChange}
+        />
+        <p>{inputs.client_id},{inputs.client_secret},{inputs.refresh_token},{inputs.investment},{inputs.risk_aversity},{inputs.ppl},{inputs.pph}</p>
+  
+        <input type="submit" />
+      </form>
+
+    );
   }
 
   
